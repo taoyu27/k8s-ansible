fdbe427fb1e1504cb8ca0dec0b16669e96dec593
yutao <taoyu27@126.com>
2018-09-03 10:08:10 +0800

    modify

--- script/deploy-fast-mir.sh
+++
@@ -1,9 +0,0 @@
-
-. ./init.sh
-
-
-#KEYFILES="${CUR_DIR}/../roles/ssh/files/authorized_keys"
-#cat /dev/null > KEYFILES
-#cat /root/.ssh/id_rsa.pub >> KEYFILES
-inventory=${INVENTORY:-${INVENTORY_DIR}/inventory}
-ansible_playbook ${inventory} ${PLAYBOOKS_DIR}/fast-mir.yml  "$@"
